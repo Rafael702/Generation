c87141ea0c287cd67bdad101111e197c2a5fd1d8
Rafael <rafaeldealmeida.192001@gmail.com>
2021-06-24 23:48:31 -0300

    Spirng - Blog Pessoal - Primeiros passos - Model, Repository & Controller.

---
+++ Aula24.06 - Model,Repository&Controller(Spring)/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -0,0 +1,19 @@
+package org.generation.blogPessoal.controller;
+
+import org.generation.blogPessoal.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagem") //URL
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	
+	
+}
